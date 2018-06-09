42b6f876eab02a0deabfc17f5986f37f9bc10710
Ana Jorgelina Silveira <anita_silveira@hotmail.com>
2018-06-09 18:47:39 -0300

    files added

--- Figures/Free_Energy.py
+++ Figures/Free_Energy.py
@@ -6,7 +6,7 @@ import mics
 import pandas as pd
 plt.rcParams.update({'font.size':8})
 
-fig = plt.figure('axes',figsize=(2.5,2))
+fig = plt.figure('axes',figsize=(3,2))
 ax = fig.add_subplot(111)
 ax.set_xlabel('$h$ (fs)')
 ax.set_ylabel('$\Delta F/N$ (kcal/mol)')
@@ -28,12 +28,12 @@ data = np.genfromtxt('S1.csv', delimiter=',', skip_header=1, names=[' ','T','f',
                      'dtemperatura','P','dP','E','dE','virial','dvirial','KEtotal','dKEtotal', \
                      'KEt','dKEt','KEr','dKEr','Cv1','dCv1','dfdT','ddfdT'])
 ax.errorbar(timesteps, data['f'][0:7]/(NB*beta),yerr = data['df'][0:7]/(NB*beta),linestyle ='-', color = 'black', marker = 'o',markersize = 4, linewidth = lw)
-ax2 = ax.twinx()
-ax2.set_ylim(297.0,299.0)
-yticks = [297.0,298.0,299.0]
-ax2.set_yticks(yticks)
-ax2.set_ylabel('$T$ (K)')
-ax2.errorbar(timesteps, Ts, yerr=Ts_error,linestyle ='-', color = 'red', marker = 's',markersize = 4, linewidth = lw)
+#ax2 = ax.twinx()
+#ax2.set_ylim(297.0,299.0)
+#yticks = [297.0,298.0,299.0]
+#ax2.set_yticks(yticks)
+#ax2.set_ylabel('$T$ (K)')
+#ax2.errorbar(timesteps, Ts, yerr=Ts_error,linestyle ='-', color = 'red', marker = 's',markersize = 4, linewidth = lw)
 
 
 #ax.legend(loc='upper center', bbox_to_anchor=(0.5,0.7), ncol=2, fancybox=True, frameon=False, numpoints = 1)
--- Figures/thermodynamic_properties.py
+++ Figures/thermodynamic_properties.py
@@ -15,9 +15,9 @@ ax[2].set_xlabel('$h$ (fs)')
 ax[3].set_xlabel('$h$ (fs)')
 
 ax[0].set_ylabel(r'T (K)')
-ax[1].set_ylabel(r'$<U/N>$ (kcal/mol)')
-ax[2].set_ylabel(r'$<W>/N$ (kcal/mol)')
-ax[3].set_ylabel(r'$C_v$ (kcal/mol K)')
+ax[1].set_ylabel(r'{$\langle U/N \rangle$} (kcal/mol)')
+ax[2].set_ylabel(r'{$\langle W/N \rangle$} (kcal/mol)')
+ax[3].set_ylabel(r'$C_V$ (kcal/mol K)')
 
 
 ax[1].set_xlim(0.5,7.5)
@@ -30,23 +30,33 @@ lw = 0.5
 NB = 903
 kB = 0.0019872041 #Boltzmann's constant (kcal/mol/K).
 beta = 1./(kB*298.0)
-integrators = ['S1','S1m','P1','K2']
-colors = ['blue','magenta','green','red']
-labels = ['This work (Unsplit)','This work (NO-SQUISH)', r'Martyna \textit{et al.}', r'Kamberaj \textit{et al.}']
+integrators = ['S1','S1mr','P1','K2']
+colors = ['black','magenta','green','blue']
+markers = ['s','^','o','v']
+labels = ['Refined/Unsplit','Refined/NO-SQUISH', r'Martyna', r'Kamberaj']
 k = 0
 for i in integrators:
     data = np.genfromtxt(i + '.csv', delimiter=',', skip_header=1, names=[' ','T','f','df','temperatura',\
                      'dtemperatura','P','dP','E','dE','virial','dvirial','KEtotal','dKEtotal', \
                      'KEt','dKEt','KEr','dKEr','Cv1','dCv1','dfdT','ddfdT'])
-    ax[0].errorbar(timesteps, data['temperatura'][0:7],yerr = data['dtemperatura'][0:7],linestyle ='-', color = colors[k], marker = 's',markersize = 4, linewidth = lw, label = labels[k])
-    ax[1].errorbar(timesteps, data['P'][0:7],yerr = data['dP'][0:7],linestyle ='-', color = colors[k], marker = 's',markersize = 4, linewidth = lw, label = labels[k])
-    ax[2].errorbar(timesteps, data['virial'][0:7],yerr = data['dvirial'][0:7],linestyle ='-', color = colors[k], marker = 's',markersize = 4, linewidth = lw, label = labels[k])
-    ax[3].errorbar(timesteps, data['Cv1'][0:7],yerr = data['dCv1'][0:7],linestyle ='-', color = colors[k], marker = 's',markersize = 4, linewidth = lw, label = labels[k])
+    ax[0].errorbar(timesteps, data['temperatura'][0:7],yerr = data['dtemperatura'][0:7],linestyle ='-', color = colors[k], marker = markers[k],markersize = 4, linewidth = lw, label = labels[k],capsize=2,   markeredgewidth=1)
+    ax[1].errorbar(timesteps, data['P'][0:7],yerr = data['dP'][0:7],linestyle ='-', color = colors[k], marker = markers[k],markersize = 4, linewidth = lw, label = labels[k],capsize=2,  markeredgewidth=1)
+    ax[2].errorbar(timesteps, data['virial'][0:7],yerr = data['dvirial'][0:7],linestyle ='-', color = colors[k], marker = markers[k],markersize = 4, linewidth = lw, label = labels[k],capsize=2,  markeredgewidth=1)
+    ax[3].errorbar(timesteps, data['Cv1'][0:7],yerr = data['dCv1'][0:7],linestyle ='-', color = colors[k], marker = markers[k],markersize = 4, linewidth = lw, label = labels[k],capsize=2, markeredgewidth=1)
+
     k = k + 1
+
+data = np.genfromtxt('S1wr.csv', delimiter=',', skip_header=1, names=[' ','T','f','df','temperatura',\
+                     'dtemperatura','P','dP','E','dE','virial','dvirial','KEtotal','dKEtotal', \
+                     'KEt','dKEt','KEr','dKEr','Cv1','dCv1','dfdT','ddfdT'])
+ax[0].errorbar(timesteps, data['temperatura'][0:7],yerr = data['dtemperatura'][0:7],linestyle ='-', color = 'red', marker = 'x',markersize = 4, linewidth = lw, markeredgewidth=0.5,markeredgecolor='red', markerfacecolor='None', label = 'No Reweighting')
+ax[1].errorbar(timesteps, data['P'][0:7],yerr = data['dP'][0:7],linestyle ='-', color = 'red', marker = 'x',markersize = 4, linewidth = lw, markeredgewidth=0.5,markeredgecolor='red', markerfacecolor='None', label = 'No Reweighting')
+ax[3].errorbar(timesteps, data['Cv1'][0:7],yerr = data['dCv1'][0:7],linestyle ='-', color = 'red', marker = 'x',markersize = 4, linewidth = lw, markeredgewidth=0.5,markeredgecolor='red', markerfacecolor='None', label = 'No Reweighting' )
+
 ax[0].axhline(y=298.0, color='black', linestyle='dotted',linewidth=1.0)
 ax[1].axhline(y=-9.1028, color='black', linestyle='dotted',linewidth=1.0)
 ax[2].axhline(y=-1.776, color='black', linestyle='dotted',linewidth=1.0)
 ax[3].axhline(y=17.512, color='black', linestyle='dotted',linewidth=1.0)
-ax[0].legend(loc='upper center', bbox_to_anchor=(1.1,1.2), ncol=4, fancybox=True, frameon=False, numpoints = 1)
+ax[0].legend(loc='upper center', bbox_to_anchor=(1.1,1.2), ncol=5, fancybox=True, frameon=False, numpoints = 1)
 fig.savefig('thermodynamic_properties.eps', format='eps', dpi=600, bbox_inches='tight')
 
