cc146a0e8848b1691e160d99f1b2cdf7a1bfa32a
Ana Jorgelina Silveira <anita_silveira@hotmail.com>
2018-06-10 10:13:49 -0300

    energy drift figure corrected

--- Figures/Drift.py
+++ Figures/Drift.py
@@ -10,10 +10,10 @@ from matplotlib import rc
 # activate latex text rendering
 rc('text', usetex=True)
 
-fig = plt.figure('axes',figsize=(3,2))
+fig = plt.figure('axes',figsize=(3.37,2))
 ax = fig.add_subplot(111)
 ax.set_xlabel('$h$ (fs)')
-ax.set_ylabel('$R$ [(kcal/mol) ns]')
+ax.set_ylabel('$R$ (kcal/mol.ns)')
 
 
 ax.set_xlim(0.5,10.5)
@@ -23,9 +23,9 @@ ax.set_xticks(xticks)
 lw = 0.5
 
 data = np.genfromtxt('Drift.csv', delimiter=',', skip_header=1, names=['timestep','thermo','Hs_nhc_thiswork','Hs_thiswork','H_nhc_martyna','Hs_nve'])
-ax.plot(data['timestep'], data['Hs_nhc_thiswork']/data['thermo'],linestyle ='-', color = 'black', marker = 'v',markersize = 4, linewidth = lw, label = r'$\widetilde{H}$ (This work) ')
-ax.plot(data['timestep'], data['H_nhc_martyna']/data['thermo'],linestyle ='-', color = 'green', marker = '^',markersize = 4, linewidth = lw, label = r'$H$ (Martyna \textit{et al.}) ')
-ax.plot(data['timestep'], data['Hs_thiswork']/data['thermo'],linestyle ='-', color = 'red', marker = 's',markersize = 4, linewidth = lw,label = r'$\widetilde{\mathcal{H}}$ (This work)')
+ax.plot(data['timestep'], data['Hs_nhc_thiswork']/data['thermo'],linestyle ='-', color = 'black', marker = 'v',markersize = 4, linewidth = lw, label = r'$\widetilde{H}$ (Refined/Unsplit) ')
+ax.plot(data['timestep'], data['H_nhc_martyna']/data['thermo'],linestyle ='-', color = 'green', marker = '^',markersize = 4, linewidth = lw, label = r'$H$ (Martyna) ')
+ax.plot(data['timestep'], data['Hs_thiswork']/data['thermo'],linestyle ='-', color = 'red', marker = 's',markersize = 4, linewidth = lw,label = r'$\widetilde{\mathcal{H}}$ (Refined/Unsplit)')
 ax.plot(data['timestep'], data['Hs_nve']/data['thermo'],linestyle ='-', color = 'blue', marker = 'o',markersize = 4, linewidth = lw,label = r'$\widetilde{\mathcal{H}}$ (NVE)')
 
 ax.set_ylim(-0.5,4)
